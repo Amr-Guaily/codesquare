aa167c49ee4231b3e4acfa9aed2a0a9cdb2d8dbc
Amr-Guaily <amrguaily@gmail.com>
2025-06-24 23:36:28 +0300

    Implement Express server with CRUD endpoints for posts and integrate in-memory datastore

--- server/datastore/index.ts
+++ server/datastore/index.ts
@@ -2,5 +2,8 @@ import { CommentDAO } from './dao/commentDao';
 import { LikeDAO } from './dao/LikeDao';
 import { PostsDAO } from './dao/PostsDao';
 import { UserDAO } from './dao/UserDao';
+import { InMemoryDatastore } from './memorydb';
 
 export interface Datastore extends PostsDAO, LikeDAO, UserDAO, CommentDAO {}
+
+export const db = new InMemoryDatastore();
--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,24 @@
-console.log("hello world");
+import express, { RequestHandler } from 'express';
+import { db } from './datastore';
+const app = express();
+
+app.use(express.json());
+
+const requestLoggerMiddleware: RequestHandler = (req, res, next) => {
+  console.log(req.method, req.path, '- body:', req.body);
+  next();
+};
+
+app.use(requestLoggerMiddleware);
+
+app.get('/posts', (req, res) => {
+  res.send({ posts: db.listPosts() });
+});
+
+app.post('/posts', (req, res) => {
+  const post = req.body;
+  db.createPost(post);
+
+  res.sendStatus(201);
+});
+app.listen(3000);
