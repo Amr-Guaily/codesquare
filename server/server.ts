7a5631a8e7891a161991b84f717cb06b62488a28
Amr-Guaily <amrguaily@gmail.com>
2025-08-23 12:53:09 +0300

    feat: update production start script

--- server/server.ts
+++ server/server.ts
@@ -33,5 +33,7 @@ import { requestLoggerMiddleware } from './middleware/loggerMiddleware';
 
   app.use(errHandler);
 
-  app.listen(process.env.PORT || 3000);
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+  });
 })();
